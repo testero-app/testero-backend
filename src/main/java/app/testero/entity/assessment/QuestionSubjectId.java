6c428d76013c56434a7b92f80e05ef55ab625f28
davidefella <davidefella@users.noreply.github.com>
2026-06-15 14:14:34 +0200

    feat: add question-subject many-to-many relationship with weight
    Add question_subject and question_snapshot_subject tables via Liquibase
    migrations (v1.4). Create JPA entities, repositories, and update
    SnapshotService to copy question-subject associations (with weights)
    into snapshots. Content hash now includes subject data for change
    detection.
    
    Closes #106
    
    Signed-off-by: Davide Fella <davide@testero.app>
    Signed-off-by: davidefella <davidefella@users.noreply.github.com>

---
+++ src/main/java/app/testero/entity/assessment/QuestionSubjectId.java
@@ -0,0 +1,20 @@
+package app.testero.entity.assessment;
+
+import java.io.Serializable;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class QuestionSubjectId implements Serializable {
+
+    private UUID questionId;
+    private UUID subjectId;
+}
